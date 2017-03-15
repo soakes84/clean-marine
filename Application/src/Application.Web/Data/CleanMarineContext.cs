2d1974a281df13ada40558d944348d88ea409bf5
soakes84 <spencer.oakes84@gmail.com>
2017-03-15 11:49:43 -0400

    Created Folders, Data Models

---
+++ Application/src/Application.Web/Data/CleanMarineContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Web.Data
+{
+    public class CleanMarineContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Debris> Debris { get; set; }
+
+
+        public CleanMarineContext() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=CleanMarine.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                .ToTable("Users");
+            builder.Entity<IdentityRole>()
+                .ToTable("Roles");
+            builder.Entity<IdentityRoleClaim<string>>()
+                .ToTable("RoleClaims");
+            builder.Entity<IdentityUserClaim<string>>()
+                .ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>()
+                .ToTable("UserLogins");
+            builder.Entity<IdentityUserRole<string>>()
+                .ToTable("UserRoles");
+            builder.Entity<IdentityUserToken<string>>()
+                .ToTable("UserTokens");
+        }
+    }
+}
