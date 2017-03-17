57535f8bdb20e840661255ab7c4feb87ee5b2f46
Jerol Graves <jerolgraves@Jerols-MacBook-Pro.local>
2017-03-17 14:48:15 -0400

    first commit

--- Application/src/Application.Web/src-client/scripts/app.js
+++ Application/src/Application.Web/src-client/scripts/app.js
@@ -2,7 +2,6 @@ import Backbone from 'backbone'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import {DebrisModel} from './models/model-debris.js'
-// import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
 import {ViewController} from './viewController.js'
 
 
@@ -61,65 +60,5 @@ const AppRouter = Backbone.Router.extend({
 
 })
 
-// const coords = {
-//   lat: 51.5258541,
-//   lng: -0.08040660000006028
-// };
-//
-// const params = {v: '3.exp', key: 'YOUR_API_KEY'};
-//
-// const App = React.createClass({
-//
-//   onMapCreated(map) {
-//     map.setOptions({
-//       disableDefaultUI: true
-//     });
-//   },
-//
-//   onDragEnd(e) {
-//     console.log('onDragEnd', e);
-//   },
-//
-//   onCloseClick() {
-//     console.log('onCloseClick');
-//   },
-//
-//   onClick(e) {
-//     console.log('onClick', e);
-//   },
-//
-//   render() {
-//     return (
-//       <Gmaps
-//         width={'800px'}
-//         height={'600px'}
-//         lat={coords.lat}
-//         lng={coords.lng}
-//         zoom={12}
-//         loadingMessage={'Be happy'}
-//         params={params}
-//         onMapCreated={this.onMapCreated}>
-//         <Marker
-//           lat={coords.lat}
-//           lng={coords.lng}
-//           draggable={true}
-//           onDragEnd={this.onDragEnd} />
-//         <InfoWindow
-//           lat={coords.lat}
-//           lng={coords.lng}
-//           content={'Hello, React :)'}
-//           onCloseClick={this.onCloseClick} />
-//         <Circle
-//           lat={coords.lat}
-//           lng={coords.lng}
-//           radius={500}
-//           onClick={this.onClick} />
-//       </Gmaps>
-//     );
-//   }
-//
-// });
-
-// ReactDOM.render(<App />, document.getElementById('gmaps'));
 
 new AppRouter ()
