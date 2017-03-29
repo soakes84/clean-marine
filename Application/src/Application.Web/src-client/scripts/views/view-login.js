7118df0195b16ffc46d275c1a919d07dc942265a
Jerol Graves <jerolgraves@Jerols-MacBook-Pro.local>
2017-03-29 13:28:09 -0400

    changes

--- Application/src/Application.Web/src-client/scripts/views/view-login.js
+++ Application/src/Application.Web/src-client/scripts/views/view-login.js
@@ -11,7 +11,7 @@ export const LoginView = React.createClass({
     return (
       <div className='container'>
         <video autoplay loop muted class="background">
-        <source src="https://static.videezy.com/system/resources/previews/000/004/205/original/Waves_on_the_sand.mp4" type="video/mp4" />
+        <source src="https://static.videezy.com/system/resources/previews/000/004/205/original/Waves_on_the_sand.mp4" />
     </video>
         <LoginComponent/>
       </div>
